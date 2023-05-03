69378aee58e49c88589199bf07de36e3b302240f
Ablaye <abdoulayesoft20@gmail.com>
2023-05-03 16:04:37 +0000

    Setting up login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,14 @@ namespace App\DataFixtures;
 
 //use App\Entity\Contact;
 use Faker\Factory;
-//use App\Entity\User;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 //use App\Entity\Mark;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-//use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -73,7 +73,7 @@ class AppFixtures extends Fixture
                 ->setNbPeople(mt_rand(0, 1) == 1 ? mt_rand(1, 50) : null)
                 ->setDifficulty(mt_rand(0, 1) == 1 ? mt_rand(1, 5) : null)
                 ->setDescription($this->faker->text(300))
-                ->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
+                //->setPrice(mt_rand(0, 1) == 1 ? mt_rand(1, 1000) : null)
                 ->setIsFavorite(mt_rand(0, 1) == 1 ? true : false);
                 //->setIsPublic(mt_rand(0, 1) == 1 ? true : false)
                 //->setUser($users[mt_rand(0, count($users) - 1)]);
@@ -84,6 +84,19 @@ class AppFixtures extends Fixture
 
             //$recipes[] = $recipe;
             $manager->persist($recipe);
+        }
+
+        // Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name())
+                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                ->setEmail($this->faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+
+            $manager->persist($user);
         }
 
         // Marks
